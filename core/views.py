c4ab9986de09729ec11b7aab531ddc6de23de7fc
Kareem <fomr777@gmail.com>
2023-02-18 18:49:45 +0200

    small edits

--- core/views.py
+++ core/views.py
@@ -3,9 +3,7 @@ from django .contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, resolve_url
 from django.urls import reverse
 from .models import *
-# from users.models import User, Extended_user
 from django.http import JsonResponse
-from django.forms.models import model_to_dict
 
 # Create your views here.
 
